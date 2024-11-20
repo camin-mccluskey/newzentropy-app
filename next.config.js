b7e6c3b6dc80f2a92fd0355ee9e02902d8426d43
camin-mccluskey <mccluskey.camin@gmail.com>
2024-11-20 01:08:54 +0000

    Ignore eslint errors during builds (for now)"

--- next.config.js
+++ next.config.js
@@ -2,9 +2,14 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js";
+import './src/env.js'
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  eslint: {
+    // TODO: remove this when json-types-generator can generator non-any types
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default config;
+export default config
