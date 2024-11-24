c9f5fcfee7580985ae6e1f64af6e4b02ee7c6f37
camin-mccluskey <mccluskey.camin@gmail.com>
2024-11-24 23:44:37 +0000

    Try out analyzr

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { GeistSans } from 'geist/font/sans'
 import { type Metadata } from 'next'
 import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import { TRPCReactProvider } from '~/trpc/react'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Newzentropy',
@@ -14,6 +15,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
+      <Script
+        defer
+        data-domain="newzentropy.com"
+        src="https://getanalyzr.vercel.app/tracking-script.js"
+      />
       <body>
         <NuqsAdapter>
           <TRPCReactProvider>{children}</TRPCReactProvider>
